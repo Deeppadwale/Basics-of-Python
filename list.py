6c1c6049df773e8ad2828d800f99ecc8aa154fab
Deeppadwale <deeppadawale2001@gmail.com>
2025-06-13 12:03:25 +0530

    push code

--- list.py
+++ list.py
@@ -1,56 +1,56 @@
 # list  :
 
-# list are used to store multiple items in single variable 
-# list are one of four bilt in data types in python used to store collection of data . 
-# and other three are set , tuple ,dictionaty 
-# list are created in square brackets 
-# list items are ordered,changeble,and allow duplicate value
-# lsit items are indexed first is [0] index and second is a [1] index 
-# when we was say that the item is ordered itmeans that the item have diffrent 
-# lsit are changebale means list are add and remove items in a list  has been changed 
-# list allow duplicates 
-# list item can be any data type 
-# # lsit can be store any data type 
-# myList=["deep","nadkuma","padwale"]
-# print(f"this is the list example {myList}")
+# # # list are used to store multiple items in single variable 
+# # # list are one of four bilt in data types in python used to store collection of data . 
+# # # and other three are set , tuple ,dictionaty 
+# # # list are created in square brackets 
+# # # list items are ordered,changeble,and allow duplicate value
+# # # lsit items are indexed first is [0] index and second is a [1] index 
+# # # when we was say that the item is ordered itmeans that the item have diffrent 
+# # # lsit are changebale means list are add and remove items in a list  has been changed 
+# # # list allow duplicates 
+# # # list item can be any data type 
+# # # # lsit can be store any data type 
+# # # myList=["deep","nadkuma","padwale"]
+# # # print(f"this is the list example {myList}")
 
 
-# mygymdit=["pinut buttor","banana","apple",]
-# price=38
-# print(f"this is my GYM plane:{mygymdit} {price:.2f}")
+mygymdit=["pinut buttor","banana","apple",]
+price=38
+print(f"this is my GYM plane:{mygymdit} {price:.2f}")
 
-# # allow duplicates
+# allow duplicates
 
-# My_Account=[10000,2000,50000,4000,10000,3000.10000]
-# print(f"Alll duplicates{My_Account}")
-
-
-
-# # to dermine how meany item are present in list use len() method 
-
-# print(len(My_Account))
+My_Account=[10000,2000,50000,4000,10000,3000.10000]
+print(f"Alll duplicates{My_Account}")
 
 
-# # list item can be any data type
+
+# to dermine how meany item are present in list use len() method 
+
+print(len(My_Account))
 
 
-# my_name=['deep','nadkumar','padwale']
-# my_salary=[10000,15000,20000]
-# my_work=[True,False]
-
-# print (f"......{my_name}.......{my_salary}.......{my_work}")
+# list item can be any data type
 
 
-# #  A list can cotain any data types 
+my_name=['deep','nadkumar','padwale']
+my_salary=[10000,15000,20000]
+my_work=[True,False]
+
+print (f"......{my_name}.......{my_salary}.......{my_work}")
 
 
-# any_data_type=['deep','ajay',200,500]
-# print(any_data_type)
+# # # #  A list can cotain any data types 
+
+
+any_data_type=['deep','ajay',200,500]
+print(any_data_type)
 
 # # It is also possible to use the list() constructor when creating a new list.
 
-# lsit_constructor=list(("deep","ajay","vijay","patil"))
-# print(lsit_constructor)
+lsit_constructor=list(("deep","ajay","vijay","patil"))
+print(lsit_constructor)
 
 # # list items handel 
 
@@ -74,9 +74,9 @@ print(append_items)
 append_items.insert(1,"deep")
 print(append_items)
 
-# Extend List
+# # # Extend List
 
-# To append elements from another list to the current list, use the extend() method.
+# # # To append elements from another list to the current list, use the extend() method.
 
 x=['orange,','banana','yello','pink','salad','jk']
 
@@ -103,19 +103,19 @@ print(remove_items)
 print('deep'in remove_items)
 
 
-# create a program to add the marks in list  
+# # # create a program to add the marks in list  
 
 
-# marks=[]
-# marks1 = int(input("Enter a marks"))
-# marks.append(marks1)
+marks=[]
+marks1 = int(input("Enter a marks"))
+marks.append(marks1)
  
-# marks2= int(input("Enter a  marks "))
-# marks.append(marks2)
+marks2= int(input("Enter a  marks "))
+marks.append(marks2)
 
-# marks3=int(input("Enter a marks"))
-# marks.append(marks3)
-# print (marks)
+marks3=int(input("Enter a marks"))
+marks.append(marks3)
+print (marks)
 
 # Loop Through a List
 # You can loop through the list items by using a for loop:
@@ -135,7 +135,7 @@ for i in range(len(new_name)):
   print(new_name[i])
 
 
-  thislist = ["apple", "banana", "cherry"]
+thislist = ["apple", "banana", "cherry"]
 i = 0
 while i < len(thislist):
   print(thislist[i])
@@ -156,7 +156,7 @@ hello=["7","0""frrrrr"]
 
 
 
-# create a program to   MOst inportan program to learn coditions 
+# # # create a program to   MOst inportan program to learn coditions 
 
 
 my_lists=['apple', 'banana', 'orange', 'kivi']
@@ -169,7 +169,7 @@ for x in my_lists:
 
 print(f'{new_list}')
 
-# -------------------------------------------------------------------------------------------
+# # -------------------------------------------------------------------------------------------
 account_comprension=['Deep','is','good','programmer']
 newaccount=[x for x in account_comprension if "e" in x]
 
@@ -183,13 +183,13 @@ print (newaccounts)
 
 salary_list = [1000, 2000, 15000, 18000, 23000, 45000, 60000, 70000, "apple", "banana", "orang", "mango"]
 
-# Remove 23000
+# # Remove 23000
 adjust_for_this_month = [y for y in salary_list if y != 23000]
 
-# Keep only numbers greater than 23000
+# # Keep only numbers greater than 23000
 adjust_for_this_month_new = [y for y in salary_list if isinstance(y, (int, float)) and y > 23000]
 
-# Remove "banana"
+# # # Remove "banana"
 adjust_for = [x for x in salary_list if x != "banana"]
 
 # Remkov
@@ -207,5 +207,20 @@ print(f"replace apple with deep is good programmer{new}")
 print(f"salary >10000 :{gen_knowlege}")
 
 #-----------------------------------------------------------------------------------
+
+# list sort,copy and some operations
+# sort() is a method to use print the list desending and undesending format 
+
+
+
+
+new_salary=[x for x in salary_list if isinstance(x,(int,float))]
+xeeee=[y for y in new_salary if y > 1000 ]
+print(new_salary)
+# new_salary.sort()
+new_salary.reverse()
+print(new_salary)
+# print(new_salary)
+print(xeeee)
 
 
--- tuple.py
+++ tuple.py
@@ -1,15 +1,63 @@
-a= (1,"Deep","padwale",45,45,100,200,3000)
+# Tuple
+# Tuples are used to store multiple items in a single variable.
 
-print (f"this is the {a}")
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
 
-no =a.count(45)
-no1=a.index(45)
-print(f"new tuple {no} {no1}")
+# A tuple is a collection which is ordered and unchangeable.
+
+# Tuples are written with round brackets.
+
+# a= (1,"Deep","padwale",45,45,100,200,3000)
+
+# print (f"this is the {a}")
+
+# no =a.count(45)
+# no1=a.index(45)
+# print(f"new tuple {no} {no1}")
 
 
 
 
-a="Deep"
-b="padwale"
+# a="Deep"
+# b="padwale"
 
-print(f"this is the full name : {a} and  {b}")
+# print(f"this is the full name : {a} and  {b}")
+
+
+
+tuple_list=(0,1,2,3,4,5,6,7,8,9,"apple","banana","cherry","mango","potato","deep")
+
+
+# print(tuple_list[1])
+# print(tuple_list[2])
+# print(len(tuple_list))
+# print(tuple_list[1])
+
+# if "apple" in tuple_list :
+#     print("yes  the apple is in tuple list ")
+# if "deep " in tuple_list:
+#     print("Not in the tuple list ")
+
+
+#  # slicing 
+# tuple_list2=tuple_list[1:3]
+# print(tuple_list2)   
+
+# # append tuple 
+
+# tuple_list3=list(tuple_list)
+# tuple_list3.append("this is the ")     
+# # tuple_list3.pop(3)
+# tuple_list3[2]="deep"       
+
+# tuple_list=tuple(tuple_list3)
+
+# print(tuple_list)
+res=tuple_list.index(3,3 ,7)
+print(res)
+
+
+# print the count of the present tuple 
+
+count_tuple=tuple_list.count("deep")
+print(count_tuple)
